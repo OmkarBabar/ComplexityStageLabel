436a4b64402533b482d19b7aa4c8188b2dbbfba6
OmkarBabar <64591187+OmkarBabar@users.noreply.github.com>
2021-02-24 12:00:09 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,74 @@
+from flask import Flask, make_response, request, render_template
+import io
+from io import StringIO
+import csv
+import pandas as pd
+import numpy as np
+import pickle
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+
+filename = 'model.pkl'
+loaded_model    = pickle.load(open(filename, 'rb'))
+lb_make  = pickle.load(open('label.pkl','rb'))
+vectorizer  = pickle.load(open('vectorizer.pkl','rb'))
+
+app = Flask(__name__)
+
+all_stopwords = stopwords.words('english')
+
+lemmatizer = WordNetLemmatizer()
+
+def text_preprocessing(str_input):
+  sentences = nltk.sent_tokenize(str_input)
+  words = [lemmatizer.lemmatize(word) for word in sentences if word not in all_stopwords]
+  words = [word.lower() for word in words if word.lower() not in  all_stopwords]
+  string = ' '.join(words)
+  string = re.sub(r'[^A-Za-z]',' ',string)
+  return string
+
+def transform1(text_file_contents):
+    return text_file_contents.replace("=", ",")
+
+def make_prediction(strinput):
+	  prediction = lb_make.inverse_transform(loaded_model.predict(vectorizer.transform([text_preprocessing(strinput)])))
+
+	  predlist = prediction.tolist()
+
+	  str1 = ''.join(predlist)
+
+	  return str1
+
+@app.route('/')
+def form():
+    return render_template('index.html')
+
+@app.route('/Predict', methods=["POST"])
+def transform_view():
+    f = request.files['data_file']
+    if not f:
+        return "No file"
+
+    stream = io.StringIO(f.stream.read().decode("UTF8"), newline=None)
+    csv_input = csv.reader(stream)
+    #print("file contents: ", file_contents)
+    #print(type(file_contents))
+    print(csv_input)
+    for row in csv_input:
+        print(row)
+
+    stream.seek(0)
+    result = transform1(stream.read())
+
+    df = pd.read_csv(StringIO(result))
+	
+	df['prediction'] = df['Solution'].apply(make_prediction)
+
+    response = make_response(df.to_csv())
+    response.headers["Content-Disposition"] = "attachment; filename=result.csv"
+    return response
+
+if __name__ == "__main__":
+    app.run(debug=True)
