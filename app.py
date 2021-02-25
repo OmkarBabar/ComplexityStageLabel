59729ea6dc61ae820c94891bfa26eb580348464f
OmkarBabar <64591187+OmkarBabar@users.noreply.github.com>
2021-02-25 15:39:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -63,8 +63,8 @@ def transform_view():
 
     df = pd.read_csv(StringIO(result), usecols=[1])
 
-    #df['prediction'] = df['Solution'].apply(make_prediction)
-    df['prediction'] = df.apply(make_prediction)
+    df['prediction'] = df['Solution'].apply(make_prediction)
+    #df['prediction'] = df.apply(make_prediction)
 
     #response = make_response(df.to_csv())
     response = make_response(df.to_csv("predict.csv", index=False, header=False, encoding='utf8'))
