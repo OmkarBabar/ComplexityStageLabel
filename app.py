6b79a29af5e8bc4e65f031845c665e436995079e
OmkarBabar <64591187+OmkarBabar@users.noreply.github.com>
2021-02-26 11:17:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def form():
 @app.route('/data', methods=['POST'])
 def data():
 	if request.method == 'POST':
-		f = request.form['csvfile']
+		f = request.files['csvfile']
 		if not f:
 			return "No file"
 		
