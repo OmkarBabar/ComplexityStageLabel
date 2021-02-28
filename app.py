1a35940ec2215be404a5f2c4a02b9453b9ba5b6b
OmkarBabar <64591187+OmkarBabar@users.noreply.github.com>
2021-02-28 19:13:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import numpy as np
 import pickle
 import re
 import nltk
-#from nltk.corpus import stopwords
+from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 import codecs
 import os
@@ -15,23 +15,8 @@ import flask_restful as restful
 from werkzeug.utils import secure_filename
 import boto3
 
-#nltk.download('stopwords')
-
-all_stopwords= ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've",\
-            "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', \
-            'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their',\
-            'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', \
-            'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', \
-            'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', \
-            'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after',\
-            'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further',\
-            'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more',\
-            'most', 'other', 'some', 'such', 'only', 'own', 'same', 'so', 'than', 'too', 'very', \
-            's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', \
-            've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn',\
-            "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn',\
-            "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", \
-            'won', "won't", 'wouldn', "wouldn't"]
+nltk.download('stopwords')
+nltk.download('punkt')
 
 filename = 'model.pkl'
 loaded_model    = pickle.load(open(filename, 'rb'))
@@ -46,7 +31,7 @@ ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
 
 app = Flask(__name__)
 
-#all_stopwords = stopwords.words('english')
+all_stopwords = stopwords.words('english')
 
 lemmatizer = WordNetLemmatizer()
 
