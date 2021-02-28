2d4941613a5f6751fbb923d23e38d2fcf71479ae
OmkarBabar <64591187+OmkarBabar@users.noreply.github.com>
2021-02-28 10:34:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -69,6 +69,11 @@ def data():
 		print('AWS_SECRET_ACCESS_KEY',AWS_SECRET_ACCESS_KEY)
 		
 		client = boto3.client('s3',aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+		
+		if client:
+			print('Success..............')
+		else:
+			print('Failed..............')
 	
 		file = request.files['csvfile']
 		filename = secure_filename(file.filename)
