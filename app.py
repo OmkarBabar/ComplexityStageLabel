8fe068ddf562fcbe904774f30aab3d670598c6b0
OmkarBabar <64591187+OmkarBabar@users.noreply.github.com>
2021-02-28 19:14:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -116,9 +116,3 @@ def data():
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port = 8080)
-	"""
-	app.debug = True
-	port = int(os.environ.get('PORT', 5000))
-	app.run(host='0.0.0.0', port = port)
-	#app.run(debug=True)
-	"""
