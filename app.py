ee58773ae8be00c281ff00c53d82e56e00f54976
OmkarBabar <64591187+OmkarBabar@users.noreply.github.com>
2021-02-28 13:57:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -73,7 +73,8 @@ def data():
 		print('AWS_ACCESS_KEY_ID',AWS_ACCESS_KEY_ID)
 		print('AWS_SECRET_ACCESS_KEY',AWS_SECRET_ACCESS_KEY)
 		
-		s3 = boto3.client('s3')
+		#s3 = boto3.client('s3')
+		s3 = boto3.resource('s3')
 		
 		#client = boto3.client('s3',aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
 		
